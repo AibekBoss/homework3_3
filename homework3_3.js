a54c09d084c9de6894075ba90e6691be115445ad
Пользователь <AibekBoss>
2021-07-31 09:21:25 +0600

    hell

---
+++ homework3_3.js
@@ -0,0 +1,91 @@
+/// >>>>>>>>>>>>>>>>>_1-зажание_>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+// let user = [
+//     { name: "John", age: 20 },
+//     { name: "Brad", age: 15 },
+//     { name: "Sunny", age: 22 },
+//     { name: "Joseph", age: 34 },
+//     { name: "Harry", age: 10 },
+//     { name: "Antony", age: 52 },
+//     { name: "John", age: 16 },
+// ];
+// const a = user.filter((user)=> user.age>18)
+// console.log(a)
+// >>>>>>>>>>>>>>>>_2-задание _>>>>>>>>>>>>>>>>>
+// let user = [
+//     { name: "John", age: 20 },
+//     { name: "Brad", age: 15 },
+//     { name: "Sunny", age: 22 },
+//     { name: "Joseph", age: 34 },
+//     { name: "Harry", age: 10 },
+//     { name: "Antony", age: 52 },
+//     { name: "John", age: 16 },
+// ];
+
+// const UpWork = user.map((neme)=>{
+//     console.log('USERNAME:',neme.name)
+//     console.log('AGE:',neme.age)
+// })
+
+// >>>>>>>>>>>>>>_3-задание >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+// let user = [
+//     { name: "John", age: 20 },
+//     { name: "Brad", age: 15 },
+//     { name: "Sunny", age: 22 },
+//     { name: "Joseph", age: 34 },
+//     { name: "Harry", age: 10 },
+//     { name: "Antony", age: 52 },
+//     { name: "John", age: 16 },
+// ];
+
+
+// const onlyDevs = user.filter(dev => dev.name.includes('J'))
+// console.log(onlyDevs)
+
+
+// >>>>>>>>>>>>>>>>_4_задание >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+// let user = [
+//     { name: "John", age: 20 },
+//     { name: "Brad", age: 15 },
+//     { name: "Sunny", age: 22 },
+//     { name: "Joseph", age: 34 },
+//     { name: "Harry", age: 10 },
+//     { name: "Antony", age: 52 },
+//     { name: "John", age: 16 },
+// ];
+
+
+// const onlyDevs = user.filter(dev => dev.name.includes('J'))
+// console.log(onlyDevs)
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>_5_задание>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+// let user = [
+//     { name:  "Bohn", age: 20 },
+//     { name: "Brad", age: 15 },
+//     { name: "Sunny", age: 22 },
+//     { name: "Joseph", age: 34 },
+//     { name: "Barry", age: 10 },
+//     { name: "Antony", age: 52 },
+//     { name: "Jouhn", age: 16 },
+// ];
+
+// const one = user.filter((af)=>af.name[0]=='B') // меняйте букву здесь
+
+// const two = one.reduce((sum,cur)=>{
+//        return sum+cur.name.length
+// },"")
+
+// const rass = two.split('')
+// console.log(rass)
+
+
+
+
+
+
+
+
+
+
